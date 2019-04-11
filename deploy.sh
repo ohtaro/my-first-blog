01c01b8dc6d183a7097c06544c637bf0de621c20
ohta yusuke <ohta.yusuke@nttcom.co.jp>
2019-04-11 09:57:12 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Exit on any error
 set -e
-envsubst < ./k8s/kustomization-template.yaml > "./k8s/base/kustomization.yaml"
+#envsubst < ./k8s/kustomization-template.yaml > "./k8s/base/kustomization.yaml"
 
-gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/${IMAGE_NAME}:$CIRCLE_SHA1
-
-kubectl kustomize "./k8s/base" | kubectl apply -f -
+kubectl -k ./k8s/base

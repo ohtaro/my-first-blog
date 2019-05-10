3b70f72eb6373552c4c2e7363516fc540fa90dad
ohta yusuke <ohta.yusuke@nttcom.co.jp>
2019-05-10 22:46:20 +0900

    fix

--- docker/docker-django/entrypoint.sh
+++ docker/docker-django/entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 python manage.py flush --no-input
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
-ddtrace-run python manage.py runserver 0.0.0.0:80
+ddtrace-run uwsgi --ini uwsgi.ini
